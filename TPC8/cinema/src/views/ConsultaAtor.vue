41c6550990ad26b646bd828f29ab26c6affaad1b
Tiago Sá <tiagosa95@gmail.com>
2019-05-07 21:15:37 +0100

    Added TPC8

---
+++ TPC8/cinema/src/views/ConsultaAtor.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-container>
+        <Info :idAtor="$route.params.id"/>
+        <FilmesAtor :idAtor="$route.params.id"/>
+        <v-layout>
+            <v-btn color="info" @click="$router.go(-1)">
+                Voltar à página anterior
+            </v-btn>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+import Info from '@/components/Info'
+import FilmesAtor from '@/components/FilmesAtor'
+
+export default {
+  components: {
+    Info, FilmesAtor
+  }
+}
+</script>
---
+++ TPC8/cinema/src/views/ListaFilmes.vue
@@ -0,0 +1,13 @@
+<template>
+    <Filmes/>
+</template>
+
+<script>
+
+import Filmes from '@/components/Filmes.vue'
+export default {
+  components: {
+    Filmes
+  }
+}
+</script>
